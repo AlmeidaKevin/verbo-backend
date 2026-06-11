77be3da3af1a5b522aaf3adf5937f0ee5da49a7b
AlmeidaKevin <almeidakevin783@gmail.com>
2026-06-11 15:57:48 -0500

    Update asistenciaRoutes.js

--- routes/asistenciaRoutes.js
+++ routes/asistenciaRoutes.js
@@ -4,9 +4,12 @@ const ctrl = require('../controllers/asistenciaController');
 const { proteger } = require('../middleware/authMiddleware');
 
 router.post('/registro', proteger, ctrl.iniciarRegistro);
+router.get('/registros-del-dia', proteger, ctrl.registrosDelDia);
 router.post('/marcar', proteger, ctrl.marcarAsistencia);
 router.put('/:id', proteger, ctrl.editarAsistencia);
+router.put('/registro/:id/observacion', proteger, ctrl.actualizarObservacion);
 router.post('/guardar', proteger, ctrl.guardarRegistro);
 router.get('/historial', proteger, ctrl.historialRegistros);
-router.get('/exportar/:registro_id', proteger, ctrl.exportarExcel);
+router.get('/exportar/:registro_id', proteger, ctrl.exportar);
+
 module.exports = router;
