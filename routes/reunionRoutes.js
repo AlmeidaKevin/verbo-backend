1c85ddeee57b12f82add9c9e4fb0335da6c6d9e7
AlmeidaKevin <almeidakevin783@gmail.com>
2026-06-17 14:04:19 -0500

    Update reunionRoutes.js

--- routes/reunionRoutes.js
+++ routes/reunionRoutes.js
@@ -4,7 +4,7 @@ const r1 = express.Router();
 const ctrl1 = require('../controllers/reunionController');
 const { proteger, soloAdmin } = require('../middleware/authMiddleware');
 const { reglasReunion, validar } = require('../middleware/validarMiddleware');
-r1.get('/publico', ctrl.listarReunionesPublico);
+r1.get('/publico', ctrl1.listarReunionesPublico);
 r1.get('/', proteger, ctrl1.listarReuniones);
 r1.get('/:id', proteger, ctrl1.obtenerReunion);
 r1.post('/', proteger, soloAdmin, [...reglasReunion, validar], ctrl1.crearReunion);
