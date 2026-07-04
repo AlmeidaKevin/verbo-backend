8fcde0c35e06df83cb0c3b29ce226d9bebbd5e8a
AlmeidaKevin <almeidakevin783@gmail.com>
2026-07-03 22:34:47 -0500

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const { proteger } = require('../middleware/authMiddleware');
-const { uploadFoto } = require('../middleware/uploadMiddleware');
 const {
   listarConversaciones, listarMensajes, crearOBuscarConversacion,
   enviarMensaje, marcarRecibido, contarNoLeidos, buscarUsuarios, subirArchivo,
 } = require('../controllers/chatController');
+
+const uploadChat = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
+}).single('file');
 
 router.get('/no-leidos', proteger, contarNoLeidos);
 router.get('/conversaciones', proteger, listarConversaciones);
@@ -14,6 +19,6 @@ router.get('/buscar-usuarios', proteger, buscarUsuarios);
 router.post('/conversaciones', proteger, crearOBuscarConversacion);
 router.post('/mensajes', proteger, enviarMensaje);
 router.put('/mensajes/:id/recibido', proteger, marcarRecibido);
-router.post('/subir-archivo', proteger, (req, res, next) => uploadFoto(req, res, next), subirArchivo);
+router.post('/subir-archivo', proteger, (req, res, next) => uploadChat(req, res, next), subirArchivo);
 
 module.exports = router;
